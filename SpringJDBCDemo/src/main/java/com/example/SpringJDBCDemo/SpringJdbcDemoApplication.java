a97e98fe04cd7d66d709cf34c7858d404b2729e7
milan <milanbudha007.mgr@gmail.com>
2024-01-21 17:27:23 +0545

    first commit

--- SpringJDBCDemo/src/main/java/com/example/SpringJDBCDemo/SpringJdbcDemoApplication.java
+++ SpringJDBCDemo/src/main/java/com/example/SpringJDBCDemo/SpringJdbcDemoApplication.java
@@ -2,12 +2,26 @@ package com.example.SpringJDBCDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.SpringJDBCDemo.model.Alien;
+import com.example.SpringJDBCDemo.repo.AlienRepo;
 
 @SpringBootApplication
 public class SpringJdbcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJdbcDemoApplication.class, args);
+
+		Alien alien1 = context.getBean(Alien.class);
+		alien1.setId(111);
+		alien1.setName("Milan");
+		alien1.setTech("Java");
+
+		AlienRepo repo = context.getBean(AlienRepo.class);
+		repo.save(alien1);
+
+		System.out.println(repo.findAll());
 	}
 
 }
